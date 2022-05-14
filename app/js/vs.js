f6653d57e14147f9f46ba66182fe7fcca9c8d361
Maxisso6 <maxime.colella@yahoo.com>
2022-05-14 17:30:32 +0200

    Rajout déconnexion popup dans vue VS

---
+++ app/js/vs.js
@@ -0,0 +1,9 @@
+function openForm() { //ouvrir le form de déconnexion
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() { //et le fermer 
+    document.getElementById("myForm").style.display = "none";
+}
+
+//simplement la version pour le popup de déconnexion dans la vue vs
