420ee28202632254a1b117785dc947eecac6dc0e
Paul <paullouppe@gmail.com>
2022-05-15 00:19:58 +0200

    election basique

--- app/js/vs.js
+++ app/js/vs.js
@@ -1,12 +1,52 @@
-//Versus
+//----------------------------------------------VERSUS------------------------------------------------------------
+var settings = {
+    "url": "http://bestgifapi-env.eba-mqsauu4a.us-east-1.elasticbeanstalk.com/api/election/10",
+    "method": "GET",
+    "timeout": 0,
+};
+let i = 0;
+$.ajax(settings).done(function(response) {
+    play(response);
+});
+
+function play(response) {
+    updateAffichage(response.candidats[Math.floor(Math.random() * response.candidats.length)], response.candidats[Math.floor(Math.random() * response.candidats.length)], response);
+}
+
+function updateAffichage(candidat1, candidat2, response) {
+    $("#button_choix1").click(function() {
+        vote(candidat1, response);
+    });
+    $("#button_choix2").click(function() {
+        vote(candidat2, response);
+    });
+    $("#img_choix1").attr("src", candidat1.url);
+    $("#img_choix2").attr("src", candidat2.url);
+    i++;
+}
+
+function vote(candidat, response) {
+    /*let newscore = candidat.score + 1;
+    var settings = {
+        "url": "http://bestgifapi-env.eba-mqsauu4a.us-east-1.elasticbeanstalk.com/api/candidat/" + candidat.id + "?score=" + newscore,
+        "method": "PUT",
+        "timeout": 0,
+    };
+
+    $.ajax(settings).done(function(res) {
+    });*/
+    if (i > (response.candidats.length * 2)) {
+        window.location.href = "./election_utilisateur.html";
+    } else {
+        updateAffichage(response.candidats[Math.floor(Math.random() * response.candidats.length)], response.candidats[Math.floor(Math.random() * response.candidats.length)], response);
+    }
+
+}
 
 
-
-
-
-
-//popup
+//----------------------------------------------POPUP------------------------------------------------------------
 $("#btn_deconnexion").click((e) => {
+    $("#buttonch")
     window.location.href = "./connexion.html"
 })
 
